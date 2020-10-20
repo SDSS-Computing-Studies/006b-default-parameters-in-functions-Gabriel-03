10acbb1c9ccb0e816b664edbef9e52d1d33a5544
Gabriel-03 <71287702+Gabriel-03@users.noreply.github.com>
2020-10-20 11:56:14 -0700

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -3,20 +3,48 @@
 import math
 
 def tempConversion():
-    pass
+     if unit== "C":
+    answer = (degrees * 9/5) + 35
+  if unit== "F":
+    answer = (degrees - 32) * 5/9
+    
+  answer = round(answer,1)
+  return answer
 
 def factorPair():
-    pass
+    x=a/b
+    factor_list=[]
+    factor_list.append(b)
+    factor_list.append(x)
+    return factor_list
 
+def cosineLaw(a,b,c,oppositeSide=True):
+  c = toRadians(c)
+  if oppositeSide == True:
+    ans2 = (a ** 2) + (b ** 2) - 2 * a * b * math.cos(c)
+    s = math.sqrt(ans2)
+    return s
+  else:
+    if a > b:
+      a1 = 1
+      b1 = -2 * (a) * (math.cos(c))
+      c1 = (a ** 2)-(b ** 2)
+      s = quadratic(a1,b1,c1)
+      x = solution(s)
+      return x
 
-def cosineLaw():
-    pass
+def toRadians(angled):
+  radians=(angled*math.pi)/180
+  return radians
 
-def toRadians():
-    pass
+def solution(solution):
+  solution.sort()
+  x2 = solution[1]
+  return x2
 
-def solution():
-    pass
-
-def quadratic():
-    pass
+def quadratic(a,b,c):
+  x1 = (-b + math.sqrt((b ** 2) - 4 * a * c)) / (2 * a)
+  x2 = (-b - math.sqrt((b ** 2) - 4 * a * c)) / (2 * a)
+  quadAnswer = [x1,x2]
+  quadAnswer.sort()
+  return quadAnswer
